30ddd3377cbb53cd9589b05507179e6fbd467d96
aimeebens <AimeeBENSIMON@ortmontreuil.fr>
2024-10-26 15:41:57 +0200

    Initial commit

--- src/com/mycompagny/halloworld1/helloworld.java
+++ src/com/mycompagny/halloworld1/helloworld.java
@@ -2,7 +2,7 @@ package com.mycompagny.halloworld1;
 
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World")
+        System.out.println("Hello World");
 
     }
 }
